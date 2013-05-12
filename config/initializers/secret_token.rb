afb9ef6ce97764d362411ef6817d2d9d6008cfe5
= <los_populares@yahoo.com>
2013-05-11 21:51:59 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanban::Application.config.secret_token = 'f93adcc3a79b402b9b599f183f04af357f98c6071c6688ebd064299b6148a2544b3080db9eb298af1a0fe4479c44bdbd09563418419fb0b651bf1be537695379'
